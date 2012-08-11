6f4a01150c8dda45b5e34af4d9e2638ad4ec7b3a
jfsantos <joao.eel@gmail.com>
2012-08-11 13:05:38 -0400

    Added personal settings for ess-knitr and AUCTeX (Sumatra as default reader)

---
+++ personal/init.el
@@ -0,0 +1,24 @@
+;; Add support for knit and purl
+(add-to-list 'load-path "~/.emacs.d/personal/")
+(require 'ess-knitr)
+
+;; LaTeX configuration
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+(setq TeX-PDF-mode t)
+
+;; Setting up SumatraPDF as default reader and configuring SyncTeX
+(setq TeX-source-correlate-method (quote synctex))
+(setq TeX-source-correlate-mode t)
+(setq TeX-source-correlate-start-server t)
+(setq TeX-view-program-list '(("SumatraPDF" "\"C:/Program Files (x86)/SumatraPDF/SumatraPDF.exe\" -reuse-instance %o")))
+(setq TeX-view-program-selection '((output-pdf "SumatraPDF")))
+(require 'sumatra-forward)
